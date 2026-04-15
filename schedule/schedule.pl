01681d4ecdc8e1314dcb8a3a437770daf815e33b
Max Maischein <corion@corion.net>
2026-04-15 19:39:29 +0200

    Always use current year

--- schedule/schedule.pl
+++ schedule/schedule.pl
@@ -15,9 +15,10 @@ use Mojo::Util qw(slugify);
 use Text::CSV_XS;
 
 my $schedule_csv = $ARGV[0] || 'export_talks';
+my $year         = 1900+(localtime())[3];
 my $csv          = Text::CSV_XS->new({ sep_char => ',', binary => 1 });
 my $uuid         = Data::UUID->new;
-my $base_url     = 'https://act.yapc.eu/gpw2024/';
+my $base_url     = "https://act.yapc.eu/gpw$year/";
 my $tz           = '+02:00';
 
 my %data;
